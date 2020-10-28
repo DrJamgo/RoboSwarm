ee2215eb22f0dd00bc655a0deb8b564ec26173fc
DrJamgo <drjamgo@hotmail.com>
2020-10-28 20:21:34 +0100

    FEAT: Isometric basic blocks and ramps

---
+++ iso_16x16.lua
@@ -0,0 +1,131 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.2.4",
+  orientation = "isometric",
+  renderorder = "right-down",
+  width = 12,
+  height = 12,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 23,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "iso_16x24",
+      firstgid = 1,
+      filename = "iso_16x24.tsx",
+      tilewidth = 16,
+      tileheight = 24,
+      spacing = 0,
+      margin = 0,
+      columns = 8,
+      image = "iso_16x24.png",
+      imagewidth = 128,
+      imageheight = 192,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 16,
+        height = 24
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 64,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      id = 18,
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 12,
+      height = 12,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 11, 11, 11, 11, 11, 11, 11, 11, 0, 0,
+        0, 0, 11, 11, 11, 11, 11, 11, 11, 11, 0, 0,
+        0, 0, 11, 11, 19, 17, 17, 10, 11, 11, 0, 0,
+        0, 0, 11, 11, 21, 14, 14, 1, 11, 11, 0, 0,
+        0, 0, 11, 11, 21, 14, 14, 1, 11, 11, 0, 0,
+        0, 0, 11, 11, 12, 5, 5, 3, 11, 11, 0, 0,
+        0, 0, 11, 11, 11, 11, 11, 11, 11, 11, 0, 0,
+        0, 0, 11, 11, 11, 11, 11, 11, 11, 11, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "tilelayer",
+      id = 21,
+      name = "Tile Layer 2",
+      x = 0,
+      y = 0,
+      width = 12,
+      height = 12,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = -8,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 11, 11, 0, 0, 0, 0, 0,
+        0, 0, 0, 11, 19, 17, 17, 10, 11, 0, 0, 0,
+        0, 0, 11, 19, 0, 0, 0, 0, 10, 11, 0, 0,
+        0, 0, 19, 0, 0, 0, 0, 0, 0, 10, 0, 0,
+        0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 12, 0, 0, 0, 0, 0, 0, 3, 0, 0,
+        0, 0, 11, 12, 0, 0, 0, 0, 3, 11, 0, 0,
+        0, 0, 0, 11, 12, 5, 5, 3, 11, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "tilelayer",
+      id = 22,
+      name = "Tile Layer 3",
+      x = 0,
+      y = 0,
+      width = 12,
+      height = 12,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = -16,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 11, 11, 11, 19, 17, 17, 10, 11, 11, 11, 0,
+        0, 11, 11, 19, 18, 0, 0, 9, 10, 11, 11, 0,
+        0, 11, 19, 18, 0, 0, 0, 0, 9, 10, 11, 0,
+        0, 11, 21, 0, 0, 0, 0, 0, 0, 1, 11, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    }
+  }
+}
---
+++ iso_16x24.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="iso_16x24" tilewidth="16" tileheight="24" tilecount="64" columns="8">
+ <image source="iso_16x24.png" width="128" height="192"/>
+</tileset>
