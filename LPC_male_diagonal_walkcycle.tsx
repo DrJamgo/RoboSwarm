056a0ad9b57a57d826e406e7b7665b71b9494aa1
DrJamgo <drjamgo@hotmail.com>
2020-12-07 20:46:42 +0100

    CLN: Cleanup

--- LPC_male_diagonal_walkcycle.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="LPC_male_diagonal_walkcycle" tilewidth="32" tileheight="32" tilecount="72" columns="9">
- <tileoffset x="0" y="-4"/>
- <grid orientation="orthogonal" width="64" height="64"/>
- <image source="LPC_male_diagonal_walkcycle.png" width="288" height="256"/>
-</tileset>
